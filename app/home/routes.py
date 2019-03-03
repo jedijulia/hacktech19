8a8b17be24e4fdb4f08f822b7fac2761f399f574
Marton Demeter <marton1029@gmail.com>
2019-03-03 00:32:09 -0800

    hackermannnnn

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,13 +1,10 @@
 import json
 
 from app.home import blueprint
-from flask import render_template,request
+from flask import render_template,request,redirect,url_for,jsonify
 from flask_login import login_required
 
 from app.home import ebayclass
-
-
-
 
 @blueprint.route('/index',methods=['GET', 'POST'])
 @login_required
@@ -24,14 +21,23 @@ def index():
     else:
       return render_template('index.html')
 
-@blueprint.route('/<template>')
+world = "something";
+@blueprint.route('/sr',methods=['GET','POST'])
 @login_required
-def route_template(template):
-    return render_template(template + '.html',
-                           product_good_title="Burlap Bag",
+def sr():
+  global world
+  if request.method == 'POST':
+    world = request.form['hello']
+    return jsonify(redirect="/home/ei");
+
+@blueprint.route('/ei',methods=['GET','POST'])
+@login_required
+def ei():
+  if request.method == 'GET':
+    print('here')
+    return render_template('ei.html',product_good_title=world,
                            product_bad_title="Plastic Retail Bag",
                            product_good_description="1.Multi-Function eco-friendly Burlap Bags for storing small object items such as small gifts, coins, jewelry, makeup bottle cream jar and any other of your small daily items to keep all of them in a bag clean and organized.2.Comes with Unique Double Drawstring for easy fasten and avoid object item lost.3.Inner is made of soft material which prevent scratching or keeping items clean when storing.4.Wide use application: Suitable Gift Bag for Wedding Favor Gift, Crafting DIY Projects, Home use, Travelling, Beach Theme Wedding Favor, and any other occasions.",
                            product_bad_description="T-Shirt Carry-Out Bags Small Thank you bags Grocery, Convenience stores, Restaurant take out, Carry-out",
                            product_good_images=["/static/images/good-1.jpg","/static/images/good-2.jpg","/static/images/good-3.jpg","/static/images/good-4.jpg","/static/images/good-5.jpg"],
-                           product_bad_images=["/static/images/bad-1.jpg","/static/images/bad-2.jpg","/static/images/bad-3.jpg"]
-                           )
+                           product_bad_images=["/static/images/bad-1.jpg","/static/images/bad-2.jpg","/static/images/bad-3.jpg"])
