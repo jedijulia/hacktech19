66e740c04ec49626755071b50bf20e6e933c1def
Marton Demeter <marton1029@gmail.com>
2019-03-02 17:58:49 -0800

    stufffssss

--- app/home/routes.py
+++ app/home/routes.py
@@ -13,12 +13,19 @@ def index():
          ebay_obj = ebayclass.mainBay()
          #(opts, args) = ebay_obj.init_options()
          resp = ebay_obj.run(search)
-         return render_template('index.html',success_message=str(resp.json()))  
-     
+         return render_template('index.html',success_message=str(resp.json()))
+
     return render_template('index.html')
 
 
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
-    return render_template(template + '.html')
+    return render_template(template + '.html',
+                           product_good_title="Burlap Bag",
+                           product_bad_title="Plastic Retail Bag",
+                           product_good_description="1.Multi-Function eco-friendly Burlap Bags for storing small object items such as small gifts, coins, jewelry, makeup bottle cream jar and any other of your small daily items to keep all of them in a bag clean and organized.2.Comes with Unique Double Drawstring for easy fasten and avoid object item lost.3.Inner is made of soft material which prevent scratching or keeping items clean when storing.4.Wide use application: Suitable Gift Bag for Wedding Favor Gift, Crafting DIY Projects, Home use, Travelling, Beach Theme Wedding Favor, and any other occasions.",
+                           product_bad_description="T-Shirt Carry-Out Bags Small Thank you bags Grocery, Convenience stores, Restaurant take out, Carry-out",
+                           product_good_images=["/static/images/good-1.jpg","/static/images/good-2.jpg","/static/images/good-3.jpg","/static/images/good-4.jpg","/static/images/good-5.jpg"],
+                           product_bad_images=["/static/images/bad-1.jpg","/static/images/bad-2.jpg","/static/images/bad-3.jpg"]
+                           )
